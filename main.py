235a5f2ed3bb471338c8e6c634c4ef24921d3e13
Alccardi <ali.baccari@icloud.com>
2023-10-01 15:23:25 +0200

    Added median percentage of parking usages for each day

--- main.py
+++ main.py
@@ -14,7 +14,34 @@ df['day_of_week'] = df['datetime'].dt.day_name()
 parking = df['parking']
 parking_list = parking.unique().tolist()
 
+# Create Parkings List
 parking_list = ['CLEMENCEAU', 'CONDORCET', 'DARCY', 'DAUPHINE', 'GRANGIER', 'MALRAUX', 'MONGE', 'SAINTE-ANNE', 'TIVOLI', 'TREMOUILLE']
+
+# Calculate the average percentage for each day of the week for all parking locations
+avg_percentage_by_day = df.groupby(['day_of_week', 'parking'])['pourcentage'].mean().unstack().round(2)
+
+# Reorder the days of the week for proper sorting in the plot
+week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+avg_percentage_by_day = avg_percentage_by_day.reindex(week_days)
+
+print(avg_percentage_by_day)
+
+# Filter data for CLEMENCEAU and TREMOUILLE
+clem = df[df['parking'] == 'CLEMENCEAU']
+tremo = df[df['parking'] == 'TREMOUILLE']
+monge = df[df['parking'] == 'MONGE']
+
+
+# Visualise median percentage of parkings for each day
+fig, ax = plt.subplots(figsize=(16, 6))  # Create a figure and axis
+
+ax.plot(week_days, avg_percentage_by_day['CLEMENCEAU'], label='CLEMENCEAU')
+ax.plot(week_days, avg_percentage_by_day['TREMOUILLE'], label='TREMOUILLE')
+ax.plot(week_days, avg_percentage_by_day['MONGE'], label='MONGE')
+
+ax.legend()
+plt.show()
+
 
 # Replace Negative Percentage
 def replace_negatives(x):
@@ -24,10 +51,6 @@ def replace_negatives(x):
         return x
 
 df['pourcentage'] = df['pourcentage'].apply(replace_negatives)
-
-# Filter data for CLEMENCEAU and TREMOUILLE
-clem = df[df['parking'] == 'CLEMENCEAU']
-tremo = df[df['parking'] == 'TREMOUILLE']
 
 # Plotting
 fig, ax = plt.subplots(figsize=(16, 6))  # Create a figure and axis
@@ -43,15 +66,3 @@ ax.set_xlabel('Date')
 ax.set_ylabel('Percentage')
 ax.legend()
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
